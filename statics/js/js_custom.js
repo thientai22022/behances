663e54aaf1096a1bb6cdb4958d1054f33351c5fa
thientai22022 <thientai220@gmail.com>
2025-01-23 17:15:20 +0700

    update

--- statics/js/js_custom.js
+++ statics/js/js_custom.js
@@ -22,18 +22,75 @@ $(document).ready(function() {
     $('.toggle-button').on('click', function() {
         const sidebar = $('.search-sidebar');
         const toggleWrapper = $('.toggle-wrapper');
+        const grid = $('.js-grid');
+        const bar = $('.search-bar');
+    
         if (sidebar.is(':hidden')) {
             sidebar.show(); 
             toggleWrapper.addClass('sidebarToggleButtonHidden'); 
+            grid.addClass('col3');
+            bar.addClass('active'); 
         } else {
             sidebar.hide(); 
             toggleWrapper.removeClass('sidebarToggleButtonHidden');
+            grid.removeClass('col3'); 
+            bar.removeClass('active'); 
         }
     });
+    
     $('.sidebar-header-button').on('click', function() {
         const sidebar = $('.search-sidebar');
         const toggleWrapper = $('.toggle-wrapper');
+        const grid = $('.js-grid'); 
+        const bar = $('.search-bar');
+    
         sidebar.hide(); 
         toggleWrapper.removeClass('sidebarToggleButtonHidden'); 
+        grid.removeClass('col3'); 
+        bar.removeClass('active'); 
+    });
+    const $languageSelector = $(".language-selector");
+    const $popoverContent = $(".popover-content");
+
+    // Tạo Popper.js instance
+    const popperInstance = Popper.createPopper($languageSelector[0], $popoverContent[0], {
+        placement: "bottom", // Vị trí popover
+        modifiers: [
+            {
+                name: "offset",
+                options: {
+                    offset: [0, 10], // Khoảng cách từ nút đến popover
+                },
+            },
+        ],
+    });
+
+    let isHovering = false;
+
+    $languageSelector.on("mouseenter", function () {
+        isHovering = true;
+        $popoverContent.removeAttr("hidden"); 
+        popperInstance.update(); 
+
+    $languageSelector.on("mouseleave", function () {
+        setTimeout(function () {
+            if (!isHovering) {
+                $popoverContent.attr("hidden", "true"); 
+            }
+        }, 100); 
+    });
+
+    $popoverContent.on("mouseenter", function () {
+        isHovering = true;
+        $(this).removeAttr("hidden");
+    });
+
+    $popoverContent.on("mouseleave", function () {
+        isHovering = false;
+        setTimeout(function () {
+            if (!isHovering) {
+                $popoverContent.attr("hidden", "true");
+            }
+        }, 100); 
     });
 });
