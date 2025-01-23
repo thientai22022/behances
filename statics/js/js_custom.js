dfba362c7b407a1e7a2e89a16838892b8bed9694
thientai22022 <thientai220@gmail.com>
2025-01-23 11:50:15 +0700

    update

--- statics/js/js_custom.js
+++ statics/js/js_custom.js
@@ -5,4 +5,35 @@ $(document).ready(function() {
     $('.search-input').on('click', function() {
         $('#suggestions').addClass('active');
     });
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('.search-input').length && !$(event.target).closest('#suggestions').length) {
+            $('#suggestions').removeClass('active');
+        }
+    });
+    $('.collapsible-trigger').on('click', function() {
+        const contentId = $(this).attr('aria-controls');
+        const content = $('#' + contentId);
+        const isExpanded = $(this).attr('aria-expanded') === 'true';
+        const chevron = $(this).find('.chevron-icon');
+        content.toggle();
+        $(this).attr('aria-expanded', !isExpanded);
+        chevron.toggleClass('active', !isExpanded); 
+    });
+    $('.toggle-button').on('click', function() {
+        const sidebar = $('.search-sidebar');
+        const toggleWrapper = $('.toggle-wrapper');
+        if (sidebar.is(':hidden')) {
+            sidebar.show(); 
+            toggleWrapper.addClass('sidebarToggleButtonHidden'); 
+        } else {
+            sidebar.hide(); 
+            toggleWrapper.removeClass('sidebarToggleButtonHidden');
+        }
+    });
+    $('.sidebar-header-button').on('click', function() {
+        const sidebar = $('.search-sidebar');
+        const toggleWrapper = $('.toggle-wrapper');
+        sidebar.hide(); 
+        toggleWrapper.removeClass('sidebarToggleButtonHidden'); 
+    });
 });
